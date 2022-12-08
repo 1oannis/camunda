41f043a422202fa33dca8496e2ad59ad4d81d701
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-12-08 17:07:05 +0100

    split rest request into service and controller

---
+++ src/service/customer-read.service.js
@@ -0,0 +1,29 @@
+
+
+export class CustomerReadService {
+
+  async findCustomerByName(prename, surname){
+    console.log(`** Searching Customer with id:: ${customerId} **`)
+    fetch(`http://localhost:3000/customers?prename=${prename}&surname=${surname}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+      })
+    .then(response => response.text())
+    .then(async (text)=> {
+      console.log(text)
+      const processVariables = new Variables()
+      processVariables.set("customer", text)
+
+      if(text.includes("prename")) {
+        await taskService.complete(task, processVariables);
+      }else{
+        throw 'CUSTOMER_DOES_NOT_EXIST';
+      }
+    })
+    .catch(async (error)=>{
+      await taskService.handleBpmnError(task, "CUSTOMER_DOES_NOT_EXIST", error);
+    });
+  }
+}
