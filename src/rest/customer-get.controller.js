34ef5bf7fa2c99070d67acc62fbd71695a862ba2
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-12-12 12:34:39 +0100

    make function async

--- src/rest/customer-get.controller.js
+++ src/rest/customer-get.controller.js
@@ -1,4 +1,5 @@
 import { Client, logger, Variables } from "../../node_modules/camunda-external-task-client-js/index.js"
+import { findCustomerByName } from "../service/customer-read.service.js";
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
 //  - 'logger': utility to automatically log important events
@@ -10,27 +11,7 @@ const client = new Client(config);
 // susbscribe to the topic 'GetCustomer' mentioned in the model
 client.subscribe("GetCustomer", async function({ task, taskService }) {
   const customerId = task.variables.get("id")
-  findCustomerByName()
+  findCustomerByName(prename, surname)
 
-  fetch(`http://localhost:3000/customers?id=${customerId}`, {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-    },
-  })
-  .then(response => response.text())
-  .then(async (text)=> {
-    console.log(text)
-    const processVariables = new Variables()
-    processVariables.set("customer", text)
-
-    if(text.includes("prename")) {
-      await taskService.complete(task, processVariables);
-    }else{
-      throw 'CUSTOMER_DOES_NOT_EXIST';
-    }
-  }
-    ).catch(async (error)=>{
-      await taskService.handleBpmnError(task, "CUSTOMER_DOES_NOT_EXIST", error);
-    });
+  
 });
